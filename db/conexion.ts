36ce41cfcba678473f15052ace7ae4111479c19c
alejandro silva <alejandro.silva.m@gmail.com>
2021-02-24 15:56:14 -0300

    test por error de conexion

--- db/conexion.ts
+++ db/conexion.ts
@@ -1,9 +1,9 @@
-import {Sequelize} from 'sequelize';
+import { Sequelize } from "sequelize";
 
-const db= new Sequelize('postgres_db','admin_db','admin_12345678',{
-    host:'localhost',
-    port:5432,
-    dialect:'postgres',
-})
+const db = new Sequelize(process.env.POSTGRES_DB||"postgres_db", process.env.POSTGRES_USER ||"admin_db", "admin_12345678", {
+  host:process.env.PSQL_HOST || "localhost",
+  port: 5432,
+  dialect: "postgres",
+});
 
 export default db;
--- dist/db/conexion.js
+++ dist/db/conexion.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize('postgres_db', 'admin_db', 'admin_12345678', {
-    host: '127.0.0.1',
+const db = new sequelize_1.Sequelize(process.env.POSTGRES_DB || "postgres_db", process.env.POSTGRES_USER || "admin_db", "admin_12345678", {
+    host: process.env.PSQL_HOST || "localhost",
     port: 5432,
-    dialect: 'postgres',
+    dialect: "postgres",
 });
 exports.default = db;
 //# sourceMappingURL=conexion.js.map

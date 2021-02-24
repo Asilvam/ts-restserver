45a7bea307c8c9c48eb71281adcc479750af2e67
alejandro silva <alejandro.silva.m@gmail.com>
2021-02-24 00:43:06 -0300

    mejoras conexion postgres

--- db/conexion.ts
+++ db/conexion.ts
@@ -1,6 +1,6 @@
 import {Sequelize} from 'sequelize';
 
-const db= new Sequelize('POSTGRES_DB','POSTGRES_USER','POSTGRES_PASSWORD',{
+const db= new Sequelize('postgres_db','admin_db','admin_12345678',{
     host:'localhost',
     port:5432,
     dialect:'postgres',
--- dist/db/conexion.js
+++ dist/db/conexion.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize('postgres', 'postgres', 'mysecretpassword', {
-    host: '192.168.0.13',
-    port: 5433,
+const db = new sequelize_1.Sequelize('postgres_db', 'admin_db', 'admin_12345678', {
+    host: 'localhost',
+    port: 5432,
     dialect: 'postgres',
 });
 exports.default = db;

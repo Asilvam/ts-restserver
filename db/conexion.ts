e517259bfd9ebb9cd8aa62ab04a518da6a46b3d0
alejandro silva <alejandro.silva.m@gmail.com>
2021-02-25 14:48:53 -0300

    cambio host de conexion solo heroku

--- db/conexion.ts
+++ db/conexion.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
  const db = new Sequelize(process.env.POSTGRES_DB||"postgres_db", process.env.POSTGRES_USER ||"admin_db", "admin_12345678", {
-   host:process.env.PSQL_HOST || "localhost",
+   host:"postgres",
    port: 5432,
    dialect: "postgres",
  });

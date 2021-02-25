bc2c4ef946bcbef3d7d3a0edb7ae7bad04c29700
alejandro silva <alejandro.silva.m@gmail.com>
2021-02-25 14:55:04 -0300

    limpieza codigo

--- db/conexion.ts
+++ db/conexion.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
- const db = new Sequelize(process.env.POSTGRES_DB||"postgres_db", process.env.POSTGRES_USER ||"admin_db", "admin_12345678", {
+ const db = new Sequelize("postgres_db", "admin_db", "admin_12345678", {
    host:"postgres",
    port: 5432,
    dialect: "postgres",

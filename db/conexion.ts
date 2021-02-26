f35520ee7917ad6b49cc8484fdde32e4b125f47c
alejandro silva <alejandro.silva.m@gmail.com>
2021-02-25 23:43:15 -0300

    conexiones contenedor

--- db/conexion.ts
+++ db/conexion.ts
@@ -5,5 +5,6 @@ import { Sequelize } from "sequelize";
 //    port: 5432,
 //    dialect: "postgres",
 //  });
-const db = new Sequelize('postgres://admin_db:admin_12345678@postgres:5432/postgres_db')
+let uri:string= process.env.DATABASE_URL || 'postgres://admin_db:admin_12345678@127.0.0.1:5432/postgres_db';
+const db= new Sequelize(uri);
 export default db;
--- dist/db/conexion.js
+++ dist/db/conexion.js
@@ -6,6 +6,7 @@ const sequelize_1 = require("sequelize");
 //    port: 5432,
 //    dialect: "postgres",
 //  });
-const db = new sequelize_1.Sequelize('postgres://admin_db:admin_12345678@postgres:5432/postgres_db');
+let uri = process.env.DATABASE_URL || 'postgres://admin_db:admin_12345678@127.0.0.1:5432/postgres_db';
+const db = new sequelize_1.Sequelize(uri);
 exports.default = db;
 //# sourceMappingURL=conexion.js.map

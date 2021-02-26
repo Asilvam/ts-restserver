cf7737627a30e7496dd9ffe0163d07cdf2403f99
alejandro silva <alejandro.silva.m@gmail.com>
2021-02-25 21:11:24 -0300

    mejoras de conexion postgres

--- db/conexion.ts
+++ db/conexion.ts
@@ -1,11 +1,9 @@
 import { Sequelize } from "sequelize";
 
- const db = new Sequelize("postgres_db", "admin_db", "admin_12345678", {
-   host:"postgres",
-   port: 5432,
-   dialect: "postgres",
- });
-
-//const sequelize = new Sequelize('postgres://user:pass@localhost:5432/dbname') // Example for postgres
-
+//  const db = new Sequelize("postgres_db", "admin_db", "admin_12345678", {
+//    host:"localhost",
+//    port: 5432,
+//    dialect: "postgres",
+//  });
+const db = new Sequelize('postgres://admin_db:admin_12345678@posgres:5432/postgres_db')
 export default db;
--- dist/db/conexion.js
+++ dist/db/conexion.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize(process.env.POSTGRES_DB || "postgres_db", process.env.POSTGRES_USER || "admin_db", "admin_12345678", {
-    host: process.env.PSQL_HOST || "localhost",
-    port: 5432,
-    dialect: "postgres",
-});
-//const sequelize = new Sequelize('postgres://user:pass@localhost:5432/dbname') // Example for postgres
+//  const db = new Sequelize("postgres_db", "admin_db", "admin_12345678", {
+//    host:"localhost",
+//    port: 5432,
+//    dialect: "postgres",
+//  });
+const db = new sequelize_1.Sequelize('postgres://admin_db:admin_12345678@posgres:5432/postgres_db');
 exports.default = db;
 //# sourceMappingURL=conexion.js.map

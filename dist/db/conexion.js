b7d95b048bc0f5b41c320b26968a66a63f746f25
alejandro silva <alejandro.silva.m@gmail.com>
2021-03-01 20:31:40 -0300

    heroku -

--- dist/db/conexion.js
+++ dist/db/conexion.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-//let uri:string= process.env.DATABASE_URL || 'postgres://admin_db:admin_12345678@postgres:5432/postgres_db';
-let uri = process.env.DATABASE_URL || 'postgres://admin_db:admin_12345678@127.0.0.1:5432/postgres_db';
+let uri = process.env.DATABASE_URL || 'postgres://admin_db:admin_12345678@postgres:5432/postgres_db';
+//let uri:string= process.env.DATABASE_URL || 'postgres://admin_db:admin_12345678@127.0.0.1:5432/postgres_db';
 const db = new sequelize_1.Sequelize(uri);
 exports.default = db;
 //# sourceMappingURL=conexion.js.map

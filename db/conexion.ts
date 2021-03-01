c2cc60b076d6f9a9be5246c7df63c65c15a99d38
alejandro silva <alejandro.silva.m@gmail.com>
2021-03-01 19:50:33 -0300

    heroku +

--- db/conexion.ts
+++ db/conexion.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
-let uri:string= process.env.DATABASE_URL || 'postgres://admin_db:admin_12345678@postgres:5432/postgres_db';
-//let uri:string= process.env.DATABASE_URL || 'postgres://admin_db:admin_12345678@127.0.0.1:5432/postgres_db';
+//let uri:string= process.env.DATABASE_URL || 'postgres://admin_db:admin_12345678@postgres:5432/postgres_db';
+let uri:string= process.env.DATABASE_URL || 'postgres://admin_db:admin_12345678@127.0.0.1:5432/postgres_db';
 const db= new Sequelize(uri);
 export default db;
